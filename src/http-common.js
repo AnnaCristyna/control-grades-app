26732971f16f3c2d97a4973df0ebf9c4319b2798
Anna.Barros <annacrismb@gmail.com>
2020-07-09 17:06:13 -0300

    now with Heroku post

--- src/http-common.js
+++ src/http-common.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 //Define a URL base da origem para consumo do servico
 export default axios.create({
-  baseURL: 'http://localhost:8081/',
+  baseURL: 'https://control-grades-api.herokuapp.com',
   headers: {
     'Content-type': 'application/json',
   },
